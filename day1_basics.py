901f52478e2f8116e5b8c90bf1d6f14106a271ae
agarwalkrishnali17-pixel <agarwalkrishnali17@gmail.com>
2026-01-09 20:27:58 +0530

    Create day1_basics.py

---
+++ day1_basics.py
@@ -0,0 +1,10 @@
+# My first Python program
+# Learning Python and GitHub
+
+name = "Krishnali"
+year = "2nd year IT"
+
+print("Hello!")
+print("My name is", name)
+print("I am a", year, "student")
+print("I am learning Python step by step.")
